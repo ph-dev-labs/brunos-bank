743d2c03fea9909b5a9dbc513487676a1902217b
ph-dev-labs <nwosuchimezie95@gmail.com>
2026-06-09 18:58:17 +0100

    fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,18 +34,22 @@ export default async function RootLayout({ children }: { children: React.ReactNo
         <Providers currency={currency}>{children}</Providers>
         
         {/* Smartsupp Live Chat script */}
-        <Script id="smartsupp-chat" strategy="afterInteractive">
-          {`
-            var _smartsupp = _smartsupp || {};
-            _smartsupp.key = '4296580f2931136866a6b5b0a86b2d963c4b1f83';
-            window.smartsupp||(function(d) {
-              var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
-              s=d.getElementsByTagName('script')[0];c=d.createElement('script');
-              c.type='text/javascript';c.charset='utf-8';c.async=true;
-              c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
-            })(document);
-          `}
-        </Script>
+        <Script 
+          id="smartsupp-chat" 
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _smartsupp = _smartsupp || {};
+              _smartsupp.key = '4296580f2931136866a6b5b0a86b2d963c4b1f83';
+              window.smartsupp||(function(d) {
+                var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+                s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+                c.type='text/javascript';c.charset='utf-8';c.async=true;
+                c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+              })(document);
+            `
+          }}
+        />
       </body>
     </html>
   );
